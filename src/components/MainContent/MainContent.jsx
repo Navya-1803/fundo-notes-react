07b3ae6221d95c3deb86c640eeaa574f77b77673
Navya <navya3989@gmail.com>
2026-08-27 17:44:03 +0530

    [Navya Raj] : Created initial application structure

---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Notes</h2>
+            <p>Your notes will appear here.</p>
+        </main>
+    );
+};
+
+export default MainContent;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+    return(
+        <aside>
+            <ul>
+                <li>Notes</li>
+                <li>Archive</li>
+                <li>Trash</li>
+            </ul>
+        </aside>
+    );
+};
+
+export default Sidebar;
